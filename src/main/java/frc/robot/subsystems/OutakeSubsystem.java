99363f5855cf168b88bb4b22a65d3f804da14c29
EvaSerra <eserrasierra@gmail.com>
2026-03-04 13:38:43 -0800

    Lowered max current for shooter motors

--- src/main/java/frc/robot/subsystems/OutakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/OutakeSubsystem.java
@@ -20,7 +20,7 @@ public class OutakeSubsystem extends SubsystemBase {
   /** Creates a new CANBallSubsystem. */
 public OutakeSubsystem() {
     // create brushed motors for each of the motors on the launcher mechanism
-    intakeLauncherRoller = new SparkMax(INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushed);
+    intakeLauncherRoller = new SparkMax(LAUNCHER_MOTOR_ID, MotorType.kBrushed);
     feederRoller = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushed);
 
     // create the configuration for the feeder roller, set a current limit and apply
@@ -41,8 +41,7 @@ public OutakeSubsystem() {
     // all commands using this subsystem pull values from the dashbaord to allow
     // you to tune the values easily, and then replace the values in Constants.java
     // with your new values. For more information, see the Software Guide.
-    SmartDashboard.putNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE);
-    SmartDashboard.putNumber("Intaking intake roller value", INTAKING_INTAKE_VOLTAGE);
+
     SmartDashboard.putNumber("Launching feeder roller value", LAUNCHING_FEEDER_VOLTAGE);
     SmartDashboard.putNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE);
     SmartDashboard.putNumber("Spin-up feeder roller value", SPIN_UP_FEEDER_VOLTAGE);
